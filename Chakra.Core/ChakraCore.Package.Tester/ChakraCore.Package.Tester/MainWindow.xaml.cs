62062a50551b9c9ccb125a70f235370f56e44d4c
Andrew Forster <anforste@bluejeansnet.com>
2016-12-14 09:34:36 -0800

    Latest Changes

---
+++ Chakra.Core/ChakraCore.Package.Tester/ChakraCore.Package.Tester/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+
+namespace ChakraCore.Package.Tester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            string result = "Success";
+
+            try
+            {
+                IntPtr count = new IntPtr(2);
+                Native.JsAddRef(default(IntPtr), count);
+            }
+            catch(Exception ex)
+            {
+                result = ex.Message + Environment.NewLine + ex.StackTrace;
+            }
+
+            if ( textBlock != null )
+            {
+                textBlock.Text = result;
+            }
+        }
+    }
+}
---
+++ Chakra.Core/ChakraCore.Package.Tester/ChakraCore.Package.Tester/Native.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ChakraCore.Package.Tester
+{
+    class Native
+    {
+        private const string DllName = "ChakraCore";
+
+        [DllImport(DllName, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int JsAddRef(IntPtr value, IntPtr count);
+    }
+}
